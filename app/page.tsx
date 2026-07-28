39b450d70cf167fd2a320493b07573ed8ded0b8e
Jeniffer Leong <jenifferleong119@gmail.com>
2026-07-28 11:38:31 +1000

    refactor: replace socket-listener with socket-provider #8

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import SocketListener from "./socket-listener";
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <SocketListener />
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
         <p>Hello what's up :)</p>
       </main>
