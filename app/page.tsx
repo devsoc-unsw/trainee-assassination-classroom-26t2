426a8b97129d4b50e2242b78d19f9a0f827b8e9d
sleepyjen4 <141932844+sleepyjen4@users.noreply.github.com>
2026-08-11 23:47:05 +1000

    refactor: fix apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
-        <p>Hello what's up :)</p>
+        <p>Hello what&apos;s up :)</p>
       </main>
     </div>
   );
