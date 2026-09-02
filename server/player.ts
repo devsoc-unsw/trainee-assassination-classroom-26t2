fd683ba7615dd2191900cfe58ee0dcbcdf4dee0a
henrikusmaja <henrikusmaja@users.noreply.github.com>
2026-09-02 11:27:50 +1000

    deleting unused files

--- server/player.ts
+++
@@ -1,5 +0,0 @@
-import { Player } from "../shared/types"
-
-export function createPlayer(id: string, nickname: string, colour: string, connected: boolean): Player {
-    return { id, nickname , colour, connected };
-}
